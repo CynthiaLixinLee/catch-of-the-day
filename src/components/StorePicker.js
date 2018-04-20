9c960cc9ac7a59186699a484283b2bdb61767413
Cynthia <29832823+CynthiaLixinLee@users.noreply.github.com>
2018-04-20 16:19:02 -0400

    set Header and Order state

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,13 +1,26 @@
 import React from "react";
 import { render } from "react-dom";
+import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+  goToStore = event => {
+    event.preventDefault(); // Stop the form from submitting
+    const storeName = this.myInput.value.value; // Get the input value
+    this.props.history.push(`/store/${storeName}`); // Change the page to the relevant store
+  };
   render() {
     return (
       <React.Fragment>
-        <form className="store-selector">
+        <form className="store-selector" onSubmit={this.goToStore}>
           <h2>Please Enter A Store</h2>
-          <input type="text" required placeholder="Store Name" />
+          <input
+            type="text"
+            ref={this.myInput}
+            required
+            placeholder="Store Name"
+            defaultValue={getFunName()}
+          />
           <button type="submit">Visit Store -></button>
         </form>
       </React.Fragment>
